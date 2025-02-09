102f8ff80c4b17e13701dadb58afc4fe959dc32e
mangal-9090 <karanmangal195@gmail.com>
2025-02-09 17:08:48 +0530

    added env file

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://backend-w7my.onrender.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { API_BASE_URL } from "../config";
 
-const API_URL = "http://localhost:5000/api/events";
+const API_URL = `${API_BASE_URL}/api/events`;
 
 export const fetchEvents = async () => {
   const response = await axios.get(API_URL);
